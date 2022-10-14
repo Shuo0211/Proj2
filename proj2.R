468965f5e0f5ca9508765ece7f8946a8ef8d0eef
Shuo0211 <114136342+Shuo0211@users.noreply.github.com>
2022-10-14 12:51:42 +0100

    Strategy 1

--- proj2.R
+++ proj2.R
@@ -1,15 +1,16 @@
 
-n<-5
+#strategy1
+n<-10
 v1<-c(seq(1, 2*n, by=1))
 v2<-sample(v1, 2*n, replace=FALSE)
 A<-cbind(v1,v2)
-i <- sample(v1, 1, replace=FALSE) #number of prisoner 
+i <- sample(v1, 1, replace=FALSE) #number of prisoner #start
 a<-A[i,1]
-nreps<-1
+steps<-1
 p<- a == A[i,2]
 while (p == FALSE) {
   i<-match(c(A[i,2]),v1)
   p<- a == A[i,2]
-  print(i)
-  nreps<-nreps+1
+  steps<-steps+1
 }
+
