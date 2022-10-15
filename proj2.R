c5b1a627e7d88a6238880ecbf32fd96f122f509e
hmiaosang <chenyan990826@163.com>
2022-10-16 02:26:30 +0800

    revise pone by adding 3 circles which calculate the probability

--- proj2.R
+++ proj2.R
@@ -3,6 +3,7 @@ strategy1 <- function(n,k){
   v1 <- c(seq(1, 2*n, by=1))
   v2 <- sample(v1, 2*n, replace=FALSE)
   A <- cbind(v1,v2)
+  #i <- sample(v1, 1, replace=FALSE) #number of prisoner #start
   a <- A[k,1]
   steps <- 1
   p <- a == A[k,2]
@@ -14,11 +15,12 @@ strategy1 <- function(n,k){
   return(steps)
 }
 
-#strategy 2
+#strategy 2 (Revised)
 strategy2 <- function(n,k){
   v1 <- c(seq(1, 2*n, by=1))
   v2 <- sample(v1, 2*n, replace=FALSE)
   A <- cbind(v1,v2)
+  #i <- sample(v1, 1, replace=FALSE) #number of prisoner #start
   a <- sample(v1, 1, replace=FALSE)  #number of box
   steps <- 1
   p <- A[a,2] == k
@@ -30,9 +32,10 @@ strategy2 <- function(n,k){
   return(steps)
 }
 
-#strategy 3
+#strategy 3 (Revised)
 strategy3 <- function(n,k){
   v1 <- c(seq(1, 2*n, by=1))
+  #i <- sample(v1, 1, replace=FALSE) #number of prisoner #start
   a <- sample(v1, n, replace=FALSE)
   b <- 0
   for (p in a){
@@ -54,41 +57,56 @@ strategy3 <- function(n,k){
 }
 
 
-pone<-function(n,k,strategy,nreps){
-  #give the amount of boxes, the number of prisoner, which strategy we chose and the number of 
+#The probability that prisoner k draws a card with strategy 1 and succeeds.
+circle1<-function(n,k,nreps){
   y <- rep(0,nreps)
-  if(strategy == "strategy1"){
-    for(i in 1:nreps){
-      strategy1(n,k)
-      y[i]<-strategy1(n,k)
-    }
-    x<-length(subset(y,y<=n))
-    prob=x/nreps
-    return(prob)
+  #用来记录nreps次模拟中每次囚犯查找盒子的次数
+  for(i in 1:nreps){
+    strategy1(n,k)
+    #按照选定的strategy运行，并把找到正确数字所打开的盒子数量记录到列表中
+    y[i]<-strategy1(n,k)
   }
-  
-  else if(strategy == "strategy2"){
-    for(i in 1:nreps){
-      strategy2(n,k)
-      y[i]<-strategy2(n,k)
-    }
-    x<-length(subset(y,y<=n))
-    prob=x/nreps
-    return(prob)
-  }
-  
-  else
-  {
-    for(i in 1:nreps){
-      strategy3(n,k)
-      y[i]<-strategy3(n,k)
-    }
-    x<-length(subset(y,y<=n))
-    prob=x/nreps
-    return(prob)
-  }
+  x<-length(subset(y,y<=n))
+  #将y列表中小于n次的实验摘出，并计算其个数为x
+  prob=x/nreps
+  #求成功的次数占总实验次数的比例
+  return(prob)
 }
 
+#The probability that prisoner k draws a card with strategy 2 and succeeds.
+circle2<-function(n,k,nreps){
+  y <- rep(0,nreps)
+  for(i in 1:nreps){
+    strategy2(n,k)
+    y[i]<-strategy2(n,k)
+  }
+  x<-length(subset(y,y<=n))
+  prob=x/nreps
+  return(prob)
+}
+
+#The probability that prisoner k draws a card with strategy 3 and succeeds.
+circle3<-function(n,k,nreps){
+  y <- rep(0,nreps)
+  for(i in 1:nreps){
+    strategy3(n,k)
+    y[i]<-strategy3(n,k)
+  }
+  x<-length(subset(y,y<=n))
+  prob=x/nreps
+  return(prob)
+}
+
+#1 函数pone
+pone<-function(n,k,strategy,nreps){
+  #give the amount of boxes, the number of prisoner, which strategy we chose and the number of 
+  switch(strategy,
+         strategy1 = circle1(n,k,nreps),
+         strategy2 = circle2(n,k,nreps),
+         strategy3 = circle3(n,k,nreps))
+}
+
+#2
 pall<-function(n,strategy,nreps){
   l <- c()
   b <- c(1)
